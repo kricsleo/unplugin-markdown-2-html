b65390b0d1a54a172af4f5af1dac458595e9f879
许圣峰 <kricsleo@163.com>
2023-05-12 14:44:32 +0800

    fix: types

--- src/types.ts
+++ src/types.ts
@@ -7,7 +7,7 @@ export interface Options {
   /** @see https://github.com/markdown-it/markdown-it#init-with-presets-and-options */
   markdown?: MarkdownItOptions
   /** @see https://github.com/nagaozen/markdown-it-toc-done-right#options */
-  toc?: TocOptions
+  toc?: Partial<TocOptions>
   /** @see https://github.com/valeriangalliat/markdown-it-anchor#usage */
   anchor?: markdownItAnchor.AnchorOptions
   highlight?: HighlightOptions
